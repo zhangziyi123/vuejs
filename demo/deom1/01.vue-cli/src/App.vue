cacb26a8b995266a60b2185e3b73daf64ebfc6a3
zhangziyi123 <492510851@qq.com>
2017-06-20 23:12:06 +0800

    Signed-off-by: zhangziyi123 <492510851@qq.com>

--- demo/deom1/01.vue-cli/src/App.vue
+++ demo/deom1/01.vue-cli/src/App.vue
@@ -40,25 +40,34 @@ export default {
  <!--   <Todos />-->
     <!-- 路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
-    <MyFooter />
+    <MyFooter :todos="todos" />
   </div>
 </template>
 
 <script>
+  import uuid from 'uuid'
   import MyHeader from './components/Header/Header'
   import MyFooter from './components/Footer/Footer'
+  import Store from '.store/index'
   // import Todos from './components/Todos/Todos'
   export default {
     name: 'app',
     data () {
       return {
-        todos: [], // 存储所有todos
-        visibility: 'all' // 存储当前过滤条件
+        todos: Store.todos.fetch() // 存储所有todos
       }
     },
     methods: {
       addTodo (value) {
-        console.log(value) // 输出看看子组件发送的消息内容
+        value = value && value.trim()
+        if (!value) {
+          return
+        }
+        this.todos.push({
+          id: uuid(),
+          title: value,
+          completed: false
+        })
       }
     },
     components: {
@@ -68,6 +77,15 @@ export default {
       let filter = this.$route.params.filter
       if (!filter) {
         this.$router.replace({ path: 'All' })
+      }
+    },
+    // watch todos change for localStorage persistence
+    watch: {
+      todos: {
+        handler: function (todos) {
+          Store.todos.save()
+        },
+        deep: true
       }
     }
   }
